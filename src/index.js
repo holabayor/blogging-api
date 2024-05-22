88892ccf0d8c24a18399c433e77bc7e4ab10e52f
holabayor <liasu.olabayo@gmail.com>
2024-05-22 15:52:52 +0100

    redis setup.exports = client;

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 const http = require('http');
 const app = require('./app');
 const { connectDB } = require('./config/db');
+const redis = require('./integrations/redis');
 
 // Initialize express app
 const port = parseInt(process.env.PORT || '8900');
@@ -13,6 +14,7 @@ const server = http.createServer(app);
 // Start application with DB connection
 connectDB()
   .then(() => {
+    redis.connect();
     server.listen(port, () => {
       console.log(`⚡️Server is running on port ${port}`);
     });
